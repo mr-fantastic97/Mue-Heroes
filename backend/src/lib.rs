8a38508c4980ff0239add0facb9d4e9cff0e34b2
breeze <mr-fantastic97@users.noreply.github.com>
2025-07-16 01:59:50 -0500

    Implement logic to allow Kdapp state to persist per wallet during runtime.

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-}
+//allows Kdapp state to persist per wallet during runtime.
+
+
+use std::collections::HashMap;
+use std::sync::Mutex;
+use once_cell::sync::Lazy;
+use crate::pki::PubKey;
+use crate::kdapp::MueHeroSession;
+
+pub static SESSIONS: Lazy<Mutex<HashMap<PubKey, MueHeroSession>>> =
+    Lazy::new(|| Mutex::new(HashMap::new()));
